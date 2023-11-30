cc43ba1e18d70953e619b128d79268fa801df6ec
ejmejm <ninjaedan@aol.com>
2023-11-30 10:26:40 -0700

    fp16 and include scores

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import argparse
 from fastapi import FastAPI
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from pydantic import BaseModel
@@ -7,15 +8,32 @@ import logging
 
 logger = logging.getLogger("docker_agent")
 
+def setup_arg_parser():
+    parser = argparse.ArgumentParser(description="Run the API.")
+    parser.add_argument("--full_precision", action="store_false", dest="fp16",
+                        help="Run with fp32 intead of fp16.")
+    parser.set_defaults(fp16=True)
+    return parser.parse_known_args()
+
 def main():
+    args, _ = setup_arg_parser()
+
     global app, tokenizer, model
     app = FastAPI()
 
-    model_name = os.getenv('MODEL_NAME', 'deepseek-ai/deepseek-coder-1.3b-base')
+    model_name = os.getenv("MODEL_NAME", "deepseek-ai/deepseek-coder-1.3b-base")
     logger.info(f"Using model {model_name}")
 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    dtype = torch.bfloat16 if args.fp16 else torch.float32
+    use_flash_attention = device == "cuda" and args.fp16
+
     tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-    model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True,)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name, trust_remote_code=True, use_flash_attention_2=use_flash_attention,
+        device_map=device, torch_dtype=dtype)
+    model.to(device)
+
 
 main()
 
@@ -27,7 +45,15 @@ class GenerateData(BaseModel):
 @app.put("/generate")
 def generate(item: GenerateData):
     inputs = tokenizer(item.input_text, return_tensors="pt").to(model.device)
-    outputs = model.generate(**inputs, max_length=item.max_decode_length,
-                             )
-    return {"generated_text": tokenizer.decode(outputs[0])}
+    outputs = model.generate(
+        **inputs, max_length=item.max_decode_length, return_dict_in_generate=True, output_scores=True)
+    out_tokens = outputs.sequences[0][inputs.input_ids.shape[1]:]
+    output_text = tokenizer.decode(out_tokens, skip_special_tokens=True)
 
+    # Calculate score of sequence (avg probability of the tokens)
+    transition_scores = model.compute_transition_scores(
+        outputs.sequences, outputs.scores, normalize_logits=True
+    )
+    score = torch.exp(transition_scores).mean().item()
+
+    return {"generated_text": output_text, "score": score}
--- client/call_agent.py
+++ client/call_agent.py
@@ -1,16 +1,20 @@
 import requests
 import argparse
 
-def send_request(input_text):
-    response = requests.put("http://localhost:8000/generate", json={"input_text": input_text})
-    output_data=response.json()
+def send_request(input_text, port=8000):
+    response = requests.put(f"http://localhost:{port}/generate", json={"input_text": input_text})
+    output_data = response.json()
+    output_text = output_data["generated_text"]
+    score = output_data["score"]
+
     print("Input text: " + input_text)
-    print("Generated text:")
-    print(str(output_data["generated_text"].strip()))
+    print(f"Generated text ({score:.4f}):")
+    print(output_text)
     
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Send a request to the API.')
     parser.add_argument('input_text', type=str, help='The input text to send to the API.')
+    parser.add_argument('--port', type=int, default=8000, help='Port for the API.')
     args = parser.parse_args()
-    send_request(args.input_text)
+    send_request(args.input_text, args.port)
