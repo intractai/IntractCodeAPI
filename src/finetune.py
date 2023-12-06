2953bdbb36a94c4f61ad8b08168d8b338a8974eb
ErfanMhi <mhi.erfan1@gmail.com>
2023-12-05 22:23:51 -0700

    feat: lora and qlora added.

--- src/finetune.py
+++ src/finetune.py
@@ -264,6 +264,8 @@ def train_supervised_projectdir(project_data, eval_data=None, **kwargs):
     parser = transformers.HfArgumentParser((TrainingArguments))
     training_args = parser.parse_dict(kwargs)[0]
     training_args.do_train = True
+    # TODO: this is required for training LORA and QLORA; it should be removed in the case that it harms performance
+    training_args.remove_unused_columns = False 
 
     if training_args.local_rank == 0:
         print('=' * 100)
