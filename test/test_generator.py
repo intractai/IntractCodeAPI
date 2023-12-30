23b4fba47b33bd3c898e4071ccf3bfd992ff4b90
ErfanMhi <mhi.erfan1@gmail.com>
2023-12-30 14:08:45 -0700

    test: a dummy test added as a placeholder.

---
+++ test/test_generator.py
@@ -0,0 +1,16 @@
+import pytest
+import transformers
+import torch
+from unittest.mock import Mock, patch
+
+
+@pytest.fixture
+def mock_hf_model():
+    model = Mock()
+    model.predict.return_value = 'mocked prediction'
+    return model
+
+
+@patch('transformers.PreTrainedModel', new_callable=lambda: mock_hf_model)
+def test_generator(mock_model):
+    assert True == True
